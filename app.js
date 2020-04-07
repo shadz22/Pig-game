9fecee1a44c81ba17e5ac299ef9e06a6a2ff6ca5
shadz22 <shadz22@yahoo.co.uk>
2020-04-07 14:58:41 +0100

    Swap player and reset the round-score if  number 1 is rolled

--- app.js
+++ app.js
@@ -36,6 +36,14 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer =0;
     roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
   }
 });
 
