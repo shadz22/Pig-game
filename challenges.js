6bb2840cbbe47b5ad84fae124de0b0382e191533
shadz22 <shadz22@yahoo.co.uk>
2020-04-14 18:18:11 +0100

    Challenge 3 - Add another dice to the game

--- challenges.js
+++ challenges.js
@@ -13,25 +13,21 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
   if (gamePlaying) {
-    var dice = Math.floor(Math.random() * 6) + 1;
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
   
-    var diceDom = document.querySelector('.dice');
-    diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
   
-    if (dice === 6 && lastDice === 6) {
-      scores[activePlayer] = 0;
-      document.querySelector('#score-' + activePlayer).textContent = '0';
-      nextPlayer();
-      
-    } else if ( dice !== 1) {
-      roundScore += dice;
+    if ( dice1 !== 1 && dice2 !== 1) {
+      roundScore += dice1 + dice2;
       document.getElementById('current-' + activePlayer).textContent = roundScore;
-
     } else {
       nextPlayer();
     }
-      lastDice = dice;
   }
   
 });
@@ -54,7 +50,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   
     if (scores[activePlayer] >= winningScore) { 
   
-      document.querySelector('.dice').style.display = 'none';
+      document.getElementById('dice-1').style.display = 'none';
+      document.getElementById('dice-2').style.display = 'none';
       document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
       document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
       document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -77,7 +74,8 @@ function nextPlayer() {
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -89,7 +87,8 @@ function init() {
   gamePlaying = true;
   
 
-  document.querySelector('.dice').style.display = 'none';
+  document.getElementById('dice-1').style.display = 'none';
+  document.getElementById('dice-2').style.display = 'none';
 
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
